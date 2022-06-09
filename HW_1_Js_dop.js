1fa6eaa8ea57dc9d030bd2164cd0aa3cea77d92e
Draga929 <ivan@bublikov.com>
2022-06-09 22:57:44 +0300

    add file

---
+++ HW_1_Js_dop.js
@@ -0,0 +1,104 @@
+// 26. Создать переменную “age_1” и присвоить ей значение 10
+// 27. Создать переменную “age_2” и присвоить ей значение 18
+// 28. Создать переменную “age_3” и присвоить ей значение 60
+// 29. Создать if в котором будите проверять значение переменной age_1
+// 30. Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is ” + age_1 + “ It’s less then ”
+// 31. Если age_1 >=  age_2 и age_1 <  age_3, вывести в консоль “Welcome  !”
+// 32. Если age_1  > age_3, вывести в консоль “Keep calm and look Culture channel”.
+// 33. Иначе выводите “Technical work”.
+
+// Задание 1*:
+// Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
+// Пример: const checkAge = function(age) {
+// Ваши преобразования
+// }
+// Вывести в консоль результат работы функции с возрастами 17, 18, 61  
+// function CheckAge(age) {
+// let age_2 = 18
+// let age_3 = 60
+// if (age < age_2) {
+//     console.log("You don’t have access cause your age is " + age + " It’s less then ")
+//  } else if (age >= age_2 && age < age_3) {
+//     console.log("Welcome !")
+//  } else if (age > age_3) {
+//      console.log("Keep calm and look Culture channel")
+//  } else {
+//      console.log("Technical work")
+//  }
+// }
+// CheckAge(17)
+// CheckAge(18)
+// CheckAge(61)
+
+// Задание 2*:
+// Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
+// let age = 17
+// function CheckAge(age) {
+// let age_2 = 18
+// let age_3 = 60
+// if (typeof age == "number") {
+//     if (age < age_2) {
+//     console.log("You don’t have access cause your age is " + age + " It’s less then ")
+//  } else if (age >= age_2 && age < age_3) {
+//     console.log("Welcome !")
+//  } else if (age > age_3) {
+//      console.log("Keep calm and look Culture channel")
+//  } else {
+//      console.log("Technical work")
+//  }
+//  } else {
+//     console.log("Enter a number")
+//  }
+// }
+// CheckAge(age)
+
+// Задание 3**:
+// Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
+// let age = "2"
+// function CheckAge(age) {
+//     let age_2 = 18
+//     let age_3 = 60
+//     if (/^\d+$/.test(age)) {
+//     age = +age
+//      if (age < age_2) {
+//                 console.log("You don’t have access cause your age is " + age + " It’s less then ") 
+//             } else if (age >= age_2 && age < age_3) {
+//                 console.log("Welcome !")
+//             } else if (age > age_3) {
+//                 console.log("Keep calm and look Culture channel")
+//             } else {
+//                 console.log("Technical work")
+//             } 
+//         } else {
+//           console.log("Enter a number")  
+//         }
+//  } 
+// CheckAge(age)
+
+// 4***:
+// Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке
+// Создал файл prompt.html
+{/* <script>
+    let age = prompt("Specify your age");
+    let age_2 = 18;
+    let age_3 = 60;
+    if (/^\d+$/.test(age)) {
+    age = +age
+     if (age < age_2) {
+        alert("You don’t have access cause your age is " + age + " It’s less then ") 
+            } else if (age >= age_2 && age < age_3) {
+                alert("Welcome !")
+            } else if (age > age_3) {
+                alert("Keep calm and look Culture channel")
+            } else {
+                alert("Technical work")
+            } 
+        } else {
+            alert("Enter a number")  
+        }
+</script> */}
+
+       
+
+
+
---
+++ HW_2_Js.js
@@ -0,0 +1,127 @@
+// Напишите валидационный скрипт используя функции 
+//  1. Скрипт должен на вход принимать строку.
+//  2. После проверки строки выводить в консоль сообщение где будет конкретно написано, что не так в ведённой строке.
+//  3. Минимум 5 символов в строке 
+//  4. Максимум 64 символа в строке 
+//  5. В строке должны быть буквы 
+//  6. Должна быть хотя бы одна буква в верхнем регистре 
+//  7. Должна быть хотя бы одна цифра 
+//  8. Должна быть хотя бы одна @ 
+//  9. Строка не должна быть пустой 
+
+// Программа с коментариями:
+// let text = "Введите текст"
+// typeof text == "string"
+// if (text.length >= 5) {
+//     // console.log("Верно >=5")
+// } else {
+//     console.log("Неверно, введите больше пяти символов")
+// }
+// if (text.length < 64) {
+//         // console.log("Верно <64")
+// } else {
+//     console.log("Неверно, введите меньше 64 символов")
+// }
+// if (/[a-zа-яё]/i.test(text)){
+//         // console.log("Верно, есть буквы")
+// } else {
+//     console.log("Неверно, введите хотя бы одну букву")
+// }
+// if (/[A-ZА-ЯЁ]/.test(text)) {
+//        // console.log("Верно, есть одна и боллее большой буквы")
+// } else {
+//     console.log("Неверно, введите хотя бы одну большую букву")
+// }
+// if (/[0-9]/.test(text)) {
+//         // console.log("Верно, есть хотя бы одна цифра")
+// } else {
+//     console.log("Неверно, введите хотя бы одну цифру")
+// }
+// if (/@/.test(text)) {
+//         // console.log("Верно, есть хотя бы одна @")
+// } else {
+//     console.log("Неверно, введите хотя бы одну @")
+// }
+// if (text != "") {
+//        // console.log("Верно, строка не пустая")
+// } else {
+//     console.log("Неверно, пустая строка, введите что-нибудь")
+// }
+
+//  Функция:
+let text = "Введите текст"
+function Line() {
+    if (typeof text == "string") {
+        return true
+    } else {
+        return false
+    }
+}
+function Empty() {
+    if (text.length != 0) {
+        return true
+    } else {
+        console.log("Неверно, пустая строка, введите что-нибудь")
+        }
+}
+function Min() {
+    if (text.length >= 5) {
+        return true
+    } else {
+        console.log("Неверно, введите больше 5 символов")
+    }
+}
+function Max() {
+    if (text.length < 64) {  
+        return true
+    } else {
+        console.log("Неверно, введите меньше 64 символов")
+    }
+} 
+function Letters() {
+    if (/[a-zа-яё]/i.test(text)) {
+        return true 
+    } else {
+        console.log("Неверно, введите хотя бы одну букву")
+    }
+}
+function Large_Letter() {
+    if (/[A-ZА-ЯЁ]/.test(text)) {
+        return true
+    } else {
+        console.log("Неверно, введите хотя бы одну большую букву")
+    }
+}
+function Numbers() {
+    if (/[0-9]/.test(text)) {
+        return true
+    } else {
+        console.log("Неверно, введите хотя бы одну цифру")
+    }
+}
+function At() {
+    if (/@/.test(text)) {
+        return true;
+    } else {
+        console.log("Неверно, введите хотя бы одну @")
+    }
+}
+function Empty() {
+    if (text.length != 0) {
+        return true
+    } else {
+        console.log("Неверно, пустая строка, введите что-нибудь")
+        }
+}
+if (Line() && Empty() && Min() && Max() && Letters() && Large_Letter() && Numbers() && At()) {
+    console.log("Поздравляю текст введен верно")
+} else {
+        }
+
+
+
+
+
+
+
+
